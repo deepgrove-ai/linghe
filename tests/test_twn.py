ddac16a80f435506ddcbd326cf340f92e0cd422c
Ubuntu <ubuntu@g129.voltagepark.net>
2025-12-06 02:53:10 +0000

    triton block scaled for deepseek linear

--- tests/test_twn.py
+++ tests/test_twn.py
@@ -1,5 +1,4 @@
-from linghe.quant.twn import twn_quant_tensor_fp8, twn_quant_tensor_fp8_triton
-from linghe.quant.twn import twn_quant_row_fp8, twn_quant_row_fp8_triton
+from linghe.quant.twn import twn_quant_row_fp8, twn_quant_row_fp8_triton, twn_quant_row_tensor_block_fp8_triton
 from linghe.tools.benchmark import benchmark_func
 import torch
 
@@ -15,18 +14,18 @@ def test_twn_quant_fp8(M=2048, N=768, bench=False):
 
     x = torch.randn(M, N, dtype=dtype, device=device)
 
-    x_q, alpha = twn_quant_tensor_fp8_triton(x)
-    x_q_ref, alpha_ref = twn_quant_tensor_fp8(x)
+    x_q, alpha = twn_quant_row_fp8(x)
+    x_q_ref, alpha_ref = twn_quant_row_fp8_triton(x)
+    __, _ = twn_quant_row_tensor_block_fp8_triton(x)
     print(x_q.dtype, x_q_ref.dtype)
     x_q = x_q.to(torch.float32)
     x_q_ref = x_q_ref.to(torch.float32)
     print(x_q-x_q_ref)
     print(alpha-alpha_ref)
     if bench:
-        benchmark_func(twn_quant_tensor_fp8, x, n_repeat=n_repeat, ref_bytes=M * N * 2)
-        benchmark_func(twn_quant_tensor_fp8_triton, x, n_repeat=n_repeat, ref_bytes=M * N * 2)
         benchmark_func(twn_quant_row_fp8, x, n_repeat=n_repeat, ref_bytes=M * N * 2)
         benchmark_func(twn_quant_row_fp8_triton, x, n_repeat=n_repeat, ref_bytes=M * N * 2)
+        benchmark_func(twn_quant_row_tensor_block_fp8_triton, x, n_repeat=n_repeat, ref_bytes=M * N * 2)
     
 if __name__ == "__main__":
     test_twn_quant_fp8(bench=True)
